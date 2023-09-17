8e911be614c767bdd0e8e82c54b789a57af21c7e
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2023-09-17 18:04:47 +0100

    add todo component, add line through

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { prisma } from "./db";
+import { TodoItem } from "./components/TodoItem";
 
-export default function Home(){
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+export default async function Home(){
+
+
+  const todos = await getTodos()
+
+
   return <>
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2x1">Todos</h1>
@@ -8,6 +19,10 @@ export default function Home(){
       className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
       href="/new">New</Link>
     </header>
-    <ul className="pl-4"></ul>
+    <ul className="pl-4">
+      {todos.map(todo => {
+       return <TodoItem key={todo.id}{...todo} />
+             })}
+    </ul>
   </>
 }
